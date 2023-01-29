fe9a8a861636eec95806a0508868ab848c247c13
serenity smith <serenitysmith24@gmail.com>
2023-01-29 14:05:28 -0500

    added

--- index.js
+++ index.js
@@ -1,45 +1,63 @@
+// grabbing header and creating new h1
 var head = document.querySelector('#header');
+// created new h1 element
 var newHeading = document.createElement('h1');
+// created text for h1
 var headingText = document.createTextNode('JavaScript Made This!!')
-
+// attached text to the h1
 newHeading.appendChild(headingText)
-
+// created a p element
 var newP = document.createElement('p')
+// text node to add text to p
 var newPText = document.createTextNode('wrote the JavaScript')
+// appenended text to p element
 newP.appendChild(newPText);
+//created span element
 var newSpan = document.createElement('span')
+// added text to span element 
 var spanText = document.createTextNode('Serenity')
+// gave span element a class to be styled differently
 newSpan.className = 'name'
-
+// created array of positive messages
 var newMessages = [
     'Good News',
     'Mom is Home',
     'Really?',
     'Yes Im so excited!'
 ]
-
+// grabbed messages that were on dom 
 var messages = document.getElementsByClassName('message')
+// iterated over messages to change inner html to corosponding array we created
 for (var i = 0; i < messages.length; i++) {
     messages[i].innerHTML = newMessages[i]
 }
-
+// added even listener to clear button
 document.getElementById('clear-button').addEventListener('click', function () {
+    // iterated over array to hide messages
     for (var i = 0; i < messages.length; i++) {
         messages[i].style.display = 'none'
     }
 })
+// grabbed element by id
 var dropDown = document.getElementById('theme-drop-down')
+// grabbed left messages
 var leftMessages = document.getElementsByClassName('left')
+// grabbed right messages
 var rightMessages = document.getElementsByClassName('right')
+// event listener for change drop down menu
 dropDown.addEventListener('change', function () {
+    // if value from drop down is blue/brown
     if (dropDown.value == 'theme-one') {
+        // iterated over left messages to make blue
         for (var i = 0; i < leftMessages.length; i++) {
             leftMessages[i].style.backgroundColor = 'blue'
         }
+        // iterated over rigth messages to make them brown
         for (var i = 0; i < rightMessages.length; i++) {
             rightMessages[i].style.backgroundColor = 'brown'
         }
     }
+    // 
     if (dropDown.value == 'theme-two') {
         for (var i = 0; i < leftMessages.length; i++) {
             leftMessages[i].style.backgroundColor = 'red'
@@ -65,21 +83,8 @@ document.querySelector('#send-btn').addEventListener('click', function(e) {
     counter++;
     document.querySelector('.messages').appendChild(newDiv);
 })
-// ask michael
-// added more messages using the form at the bottom.
 
 
-//Every other message will be posted on the right in one color, and the next on the left the other color.
-
-
-
-
-
-
-
-
-
-// newP.appendChild(newSpan)
 
 
 
